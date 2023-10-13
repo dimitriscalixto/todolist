8fcfb4ca2c52874cba2a9e033afd28b89df6b07d
idimitrisc47 <dimitrisc47@gmail.com>
2023-10-13 15:37:19 -0500

    DockerFile deploy

---
+++ src/main/java/br/com/dimitrisc/todolist/task/ITaskRepository.java
@@ -0,0 +1,11 @@
+package br.com.dimitrisc.todolist.task;
+
+import org.h2.util.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+    List <TaskModel> findByIdUser(UUID idUser);
+}
